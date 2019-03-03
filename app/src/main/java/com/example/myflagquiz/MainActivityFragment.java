542a49affdb6c38bf38fa2e43f034127d18be104
toivan <33359118+toivan@users.noreply.github.com>
2019-03-03 20:49:27 +0300

    03-03-19 21:00

--- app/src/main/java/com/example/myflagquiz/MainActivityFragment.java
+++ app/src/main/java/com/example/myflagquiz/MainActivityFragment.java
@@ -1,12 +1,42 @@
 package com.example.myflagquiz;
 
-import android.content.Context;
-import android.net.Uri;
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+
+import android.animation.Animator;
+import android.animation.AnimatorListener.Adapter;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.content.res.AssetManager;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.design.animation.AnimationUtils;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
+import android.os.Handler;
+import android.util.log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewAnimationUtils;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import android.content.Context;
+import android.net.Uri;
+import android.support.v4.app.Fragment;
 
 
 /**
@@ -20,12 +50,25 @@ import android.view.ViewGroup;
 public class MainActivityFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private static final String TAG = "FlagQuiz Activity";
+    private static final int FLAGS_IN_QUIZ = 10;
 
     // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private List<String> fileNameList;
+    private List<String> quizCountriesList;
+    private Set<String> regionsSet;
+    private String correctAnswer;
+    private int totalGuesses;
+    private int correctAnswers;
+    private int guessRows;
+    private SecureRandom random;
+    private Handler handler;
+    private Animation shakeAnimation;
+    private LinearLayout quizLinearLayout;
+    private TextView questionNumberTextView;
+    private ImageView flagImageView;
+    private LinearLayout[] guessLinearLayouts;
+    private TextView answerTextView;
 
     private OnFragmentInteractionListener mListener;
 
@@ -64,7 +107,49 @@ public class MainActivityFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        super.onCreateView(inflater, container, savedInstanceState);
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+
+        fileNameList = new ArrayList<>();
+        quizCountriesList = new ArrayList<>();
+        random = new SecureRandom();
+        handler = new Handler();
+
+        shakeAnimation = AnimationUtils.loadAnimation(getActivity(), R.anim.incorrect_shake);
+        shakeAnimation.setRepeatCount(3);
+
+        quizLinearLayout = (LinearLayout) view.findViewById(R.id.quizLinearLayout);
+        questionNumberTextView = (TextView) view.findViewById(R.id.questionNimberTextView);
+        flagImageView = (ImageView) view.findViewById(R.id.flagImageView);
+        guessLinearLayouts = new LinearLayout[4];
+        guessLinearLayouts[0] = (LinearLayout) view.findViewById(R.id.row1LinearLayout);
+        guessLinearLayouts[1] = (LinearLayout) view.findViewById(R.id.row2LinearLayout);
+        guessLinearLayouts[2] = (LinearLayout) view.findViewById(R.id.row3LinearLayout);
+        guessLinearLayouts[3] = (LinearLayout) view.findViewById(R.id.row4LinearLayout);
+        answerTextView = (TextView) view.findViewById(R.id.answerTextView);
+
+        for (LinearLayout row : guessLinearLayouts){
+            for (int column = 0; column < row.getChildCount(); column++){
+            Button button = (Button) row.getChildAt(column);
+            button.setOnClickListener(guessButtonListener);
+            }
+        }
+
+        questionNumberTextView.setText(getString(R.string.question, 1, FLAGS_IN_QUIZ));
+        return view;
+    }
+
+    public void updateGuessRows(SharedPreferences sharedPreferences){
+        String choices = sharedPreferences.getString(MainActivity.CHOICES, null);
+        guessRows = Integer.parseInt(choices)/2;
+
+        for (LinearLayout layout : guessLinearLayouts) layout.setVisibility(View.GONE);
+
+        for (int row = 0; row < guessRows; row++) guessLinearLayouts[row].setVisability(view.VISIBLE);
+    }
+
+    public void updateRegions(SharedPreferences sharedPreferences){
+        regionsSet = sharedPreferences.getStringSet(MainActivity.REGIONS, null);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
